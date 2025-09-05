3cb40984c0e34b9f2a87cbd8919e5046577cdd99
Stefano Amenta <git.stefano@outlook.it>
2025-09-05 17:59:01 +0200

    Experience: Aggiunto tip per indicare che con long press appare la dialog

--- src/app/components/experience/experience.component.ts
+++ src/app/components/experience/experience.component.ts
@@ -1,59 +1,57 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-experience',
-  templateUrl: './experience.component.html',
-  styleUrls: ['./experience.component.scss']
+  selector: "app-experience",
+  templateUrl: "./experience.component.html",
+  styleUrls: ["./experience.component.scss"],
 })
 export class ExperienceComponent implements OnInit {
-
   workExp: WorkExperience[] = [];
   showDialog = false;
   dialogData: any = null;
   private longPressTimeouts: any[] = [];
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
     this.workExp = [
       {
-        title: 'ZInformatica',
-        description: 'Asp .NET MVC',
+        title: "ZInformatica",
+        description: "Asp .NET MVC",
         isClicked: false,
-        dateFrom: 'mm/yy',
-        dateTo: 'mm/yy',
-        country: 'Italy'
+        dateFrom: "mm/yy",
+        dateTo: "mm/yy",
+        country: "Italy",
       },
       {
-        title: 'LHub',
-        description: 'Wordpress; HTML & CSS',
+        title: "LHub",
+        description: "Wordpress; HTML & CSS",
         isClicked: false,
-        dateFrom: 'mm/yy',
-        dateTo: 'mm/yy',
-        country: 'Italy'
+        dateFrom: "mm/yy",
+        dateTo: "mm/yy",
+        country: "Italy",
       },
       {
-        title: 'Artiso',
-        description: 'React, WPF',
+        title: "Artiso",
+        description: "React, WPF",
         isClicked: false,
-        dateFrom: 'mm/yy',
-        dateTo: 'mm/yy',
-        country: 'Germany'
+        dateFrom: "mm/yy",
+        dateTo: "mm/yy",
+        country: "Germany",
       },
       {
-        title: 'Softech',
-        description: 'Angular, Ionic, .NET core, Sql server, postGreSQL',
+        title: "Softech",
+        description: "Angular, Ionic, .NET core, Sql server, postGreSQL",
         isClicked: false,
-        dateFrom: 'mm/yy',
-        dateTo: 'mm/yy',
-        country: 'Italy'
+        dateFrom: "mm/yy",
+        dateTo: "mm/yy",
+        country: "Italy",
       },
     ];
   }
 
-
   onClickDot(experience: WorkExperience) {
-    this.workExp.forEach(exp => {
+    this.workExp.forEach((exp) => {
       exp.isClicked = false;
     });
     experience.isClicked = true;
@@ -63,7 +61,7 @@ export class ExperienceComponent implements OnInit {
     this.clearLongPressTimeout(idx);
     this.longPressTimeouts[idx] = setTimeout(() => {
       this.openDialog(exp);
-    }, 2000);
+    }, 750);
   }
 
   onDotRelease(exp: WorkExperience, idx: number) {
@@ -86,14 +84,13 @@ export class ExperienceComponent implements OnInit {
     this.showDialog = false;
     this.dialogData = null;
   }
-
 }
 
 export class WorkExperience {
-  title = '';
-  description = '';
+  title = "";
+  description = "";
   isClicked = false;
-  dateFrom = 'mm/yy';
-  dateTo = 'mm/yy';
-  country = '';
+  dateFrom = "mm/yy";
+  dateTo = "mm/yy";
+  country = "";
 }
