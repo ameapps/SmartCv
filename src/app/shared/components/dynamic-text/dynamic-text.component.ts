6db341bceec39c02298340d8c11997afc1c5d1b4
ameapps <git.stefano@outlook.it>
2025-09-15 13:30:29 +0200

    testo dinamico richiede solo un array di valori ora

--- src/app/shared/components/dynamic-text/dynamic-text.component.ts
+++ src/app/shared/components/dynamic-text/dynamic-text.component.ts
@@ -9,28 +9,30 @@ import { HomeTexts } from '../../models/home/home-texts';
 })
 export class DynamicTextComponent implements OnInit {
 
-  @Input() text = '';
+  // #region variables
+  text = '';
+  cyclesCount = 0;
+  @Input() texts: string[] = [];
+  @Input() cyclesLimit = -1;
+  @Input() config: 'title' | 'short-text' = 'short-text';
+  // #endregion
 
   constructor(public write_service: TypewriterService) { }
 
   async ngOnInit(): Promise<void> {
     setTimeout(async () => {
-      await this.writeTextAutomatically('ciaooo');
       this.cycleTexts();
     }, 500); //Timer per dare il tempo di caricare la config dagli assets
   }
 
   async cycleTexts(): Promise<void> {
-    const texts = [
-      'CODE WIZARD',
-      'SOFTWARE ENGINEER',
-      'EXPERT AI USER'
-    ];
     let index = 0;
     //Eseguo la scrittura del testo in loop 
     const loop = async () => {
-      await this.writeTextAutomatically(texts[index]);
-      index = (index + 1) % texts.length;
+      if (this.cyclesCount >= this.cyclesLimit || this.cyclesLimit === -1) return;
+      await this.writeTextAutomatically(this.texts[index]);
+      index = (index + 1) % this.texts.length;
+      ++this.cyclesCount;
       setTimeout(loop, 1000); // tempo di pausa dopo la scrittura, non fissa
     };
     // inizia il ciclo
