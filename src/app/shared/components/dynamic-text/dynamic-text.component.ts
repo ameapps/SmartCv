e7c0566c4f1021962f39663429dc8447904a36db
ameapps <git.stefano@outlook.it>
2025-09-17 13:42:34 +0200

    dynamic text: init definizione config per ogni tipo di testo

--- src/app/shared/components/dynamic-text/dynamic-text.component.ts
+++ src/app/shared/components/dynamic-text/dynamic-text.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 import { TypewriterService } from '../../services/typeWrite/type-writer-service.service';
 import { HomeTexts } from '../../models/home/home-texts';
 
@@ -7,14 +7,14 @@ import { HomeTexts } from '../../models/home/home-texts';
   templateUrl: './dynamic-text.component.html',
   styleUrls: ['./dynamic-text.component.scss']
 })
-export class DynamicTextComponent implements OnInit {
+export class DynamicTextComponent implements OnInit, OnChanges {
 
   // #region variables
   text = '';
   cyclesCount = 0;
   @Input() texts: string[] = [];
-  @Input() cyclesLimit = CyclesLimitVals.NO_LIMIT;
-  @Input() showAfterMs = showAfterMsVals.IMMEDIATLY;
+  @Input() cyclesLimit = CyclesLimitVals.NO_LIMIT as number;
+  @Input() showAfterMs = showAfterMsVals.IMMEDIATLY as number;
 
   //'intro-title' | 'hover-title' | 'short-text' = 'intro-title'
   @Input() config: string = 'intro-title';
@@ -22,8 +22,31 @@ export class DynamicTextComponent implements OnInit {
 
   constructor(public write_service: TypewriterService) { }
 
+  ngOnChanges(changes: SimpleChanges): void {
+    if(changes['config']) this.applyConfig(); 
+  }
+
   async ngOnInit(): Promise<void> {
     this.cycleTexts();
+  }
+
+  private applyConfig() {
+    if (this.config === 'intro-title') this.applyConfigIntroTitle();
+    if (this.config === 'hover-title') this.applyConfigHoverTitle();
+    if (this.config === 'short-text') this.applyConfigShortText();
+  }
+
+  applyConfigShortText() {
+    this.showAfterMs = 1000;
+    this.cyclesLimit = 999;
+  }
+
+  applyConfigIntroTitle() {
+    this.cyclesLimit = 1;
+  }
+
+  applyConfigHoverTitle() {
+    // throw new Error('Method not implemented.');
   }
 
   async cycleTexts(): Promise<void> {
