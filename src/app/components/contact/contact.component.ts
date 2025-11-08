1379b28b7b9ebab37bf9907c99a08391d28c7808
ameapps <git.stefano@outlook.it>
2025-11-08 23:59:00 +0100

    rimosso get da contacts

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -1,17 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { CommonService } from 'src/app/shared/services/common/common.service';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { TranslateService } from '@ngx-translate/core';
+import { deepClone } from 'src/app/shared/helpers/object.helper';
 
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
   styleUrls: ['./contact.component.scss']
 })
-export class ContactComponent implements OnInit {
+export class ContactComponent implements OnInit, OnDestroy {
 
-  public texts: string[] = ["CONTACTS"];
+  public texts = ["CONTACTS"];
   emailForm: FormGroup;
+  sub: any;
 
   constructor(private fb: FormBuilder, private translate: TranslateService) {
     this.emailForm = this.fb.group({
@@ -22,8 +24,12 @@ export class ContactComponent implements OnInit {
     this.subscribe();
   }
 
+  ngOnDestroy(): void {
+    this.sub.unsubscribe();
+  }
+
   private subscribe() {
-    this.translate.onLangChange.subscribe(() => {
+    this.sub = this.translate.onLangChange.subscribe(() => {
       setTimeout(() => {
         // L'esecuz. dell'evento costringe la view a controllare i bindings; riassegnare un array cambia il riferimento.
         this.texts = [this.translate.instant("PAGES.CONTACT.TITLE")];
@@ -41,6 +47,8 @@ export class ContactComponent implements OnInit {
   }
 
   ngOnInit(): void {
-
+    setTimeout(() => {
+      this.texts = [this.translate.instant("PAGES.CONTACT.TITLE")];
+    }, 1000);
   }
 }
