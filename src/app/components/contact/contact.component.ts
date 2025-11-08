4fdc710a5dfb940552451ce7283a64ec215ec4c3
ameapps <git.stefano@outlook.it>
2025-11-08 23:41:32 +0100

    rimosso get da contacts

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonService } from 'src/app/shared/services/common/common.service';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-contact',
@@ -9,15 +10,27 @@ import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 })
 export class ContactComponent implements OnInit {
 
+  public texts: string[] = ["CONTACTS"];
   emailForm: FormGroup;
 
-  constructor(private fb: FormBuilder) {
+  constructor(private fb: FormBuilder, private translate: TranslateService) {
     this.emailForm = this.fb.group({
       to: ['', [Validators.required, Validators.email]],
       subject: ['', Validators.required],
       message: ['', Validators.required],
     });
+    this.subscribe();
   }
+
+  private subscribe() {
+    this.translate.onLangChange.subscribe(() => {
+      setTimeout(() => {
+        // L'esecuz. dell'evento costringe la view a controllare i bindings; riassegnare un array cambia il riferimento.
+        this.texts = [this.translate.instant("PAGES.CONTACT.TITLE")];
+      }, 100);
+    });
+  }
+
 
   onSubmit() {
     if (this.emailForm.valid) {
